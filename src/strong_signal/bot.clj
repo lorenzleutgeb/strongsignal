b3d8006433ae540657cbf81c64894c874028ecd0
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2017-02-26 12:20:32 +0100

    Fixes

--- src/strong_signal/bot.clj
+++ src/strong_signal/bot.clj
@@ -6,7 +6,7 @@
 
 
 (defn safe-add [k c]
-  (char (+ (int \a) (mod (- (+ k (int c)) (int \a)) 26))))
+  (if (= c \ ) c (char (+ (int \a) (mod (- (+ k (int c)) (int \a)) 26)))))
 
 (defn rotate [k s]
   (map (partial safe-add k) s))
@@ -15,10 +15,10 @@
   (apply str (rotate k (seq (clojure.string/lower-case message)))))
 
 (def plaintext (clojure.string/lower-case "men generally believe what they want to believe"))
-(def k 5)
+(def k 1)
 (def ciphertext (caesar k plaintext))
 
-(def success-text "Well done! Your result is correct. As we are always in need for talents like you, make the decision to apply today. We offer a wide range of exciting roles here at ICA. Discover our very latest vacancies.")
+(def success-text "Well done! That makes sense. As we are always in need for talents like you, make the decision to apply today. We offer a wide range of exciting roles here at ICA. Discover our very latest vacancies.")
 
 (def wrong-texts '("Doesn't match! Please try again!"
                    "Sorry, that’s incorrect. Try again!"
@@ -28,9 +28,9 @@
                    "Maybe you should try again."
                    "Oh no, there is a problem. Try again!"
                    "Wrong answer, please try again."
-                   "Sorry an error occured. Try again!"
-                   "http://imgur.com/gallery/iWKad22"
-                   "http://www.odec.ca/projects/2014/jhav14a/error-code-18.jpeg"))
+                   "Sorry an error occured. Try again!"))
+                   ;"http://imgur.com/gallery/iWKad22"
+                   ;"http://www.odec.ca/projects/2014/jhav14a/error-code-18.jpeg"
 
 (defn on-message [payload]
   (println "on-message payload:")
@@ -40,7 +40,7 @@
         time-of-message (get-in payload [:timestamp])
         message-text (get-in payload [:message :text])]
     (cond
-      (s/includes? (s/lower-case message-text) ciphertext) (fb/send-message sender-id (fb/text-message success-text))
+      (s/includes? (s/lower-case message-text) plaintext) (fb/send-message sender-id (fb/text-message success-text))
       :else (fb/send-message sender-id (fb/text-message (rand-nth wrong-texts))))))
 
 (defn on-postback [payload]
