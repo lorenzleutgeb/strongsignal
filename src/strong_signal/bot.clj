0f5901e181fd80230b7b3114fcb620a2755ef0d7
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2017-02-26 12:14:44 +0100

    First proof of concept

--- src/strong_signal/bot.clj
+++ src/strong_signal/bot.clj
@@ -4,6 +4,34 @@
             [environ.core :refer [env]]
             [strong-signal.facebook :as fb]))
 
+
+(defn safe-add [k c]
+  (char (+ (int \a) (mod (- (+ k (int c)) (int \a)) 26))))
+
+(defn rotate [k s]
+  (map (partial safe-add k) s))
+
+(defn caesar [k message] message
+  (apply str (rotate k (seq (clojure.string/lower-case message)))))
+
+(def plaintext (clojure.string/lower-case "men generally believe what they want to believe"))
+(def k 5)
+(def ciphertext (caesar k plaintext))
+
+(def success-text "Well done! Your result is correct. As we are always in need for talents like you, make the decision to apply today. We offer a wide range of exciting roles here at ICA. Discover our very latest vacancies.")
+
+(def wrong-texts '("Doesn't match! Please try again!"
+                   "Sorry, that’s incorrect. Try again!"
+                   "Sorry, our experts cannot make sense of that translation!"
+                   "Hmm, that does not click, try again!"
+                   "Sorry, that’s incorrect. Try again!"
+                   "Maybe you should try again."
+                   "Oh no, there is a problem. Try again!"
+                   "Wrong answer, please try again."
+                   "Sorry an error occured. Try again!"
+                   "http://imgur.com/gallery/iWKad22"
+                   "http://www.odec.ca/projects/2014/jhav14a/error-code-18.jpeg"))
+
 (defn on-message [payload]
   (println "on-message payload:")
   (println payload)
@@ -12,10 +40,8 @@
         time-of-message (get-in payload [:timestamp])
         message-text (get-in payload [:message :text])]
     (cond
-      (s/includes? (s/lower-case message-text) "help") (fb/send-message sender-id (fb/text-message "Hi there, happy to help :)"))
-      (s/includes? (s/lower-case message-text) "image") (fb/send-message sender-id (fb/image-message "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/M101_hires_STScI-PRC2006-10a.jpg/1280px-M101_hires_STScI-PRC2006-10a.jpg"))
-      ; If no rules apply echo the user's message-text input
-      :else (fb/send-message sender-id (fb/text-message message-text)))))
+      (s/includes? (s/lower-case message-text) ciphertext) (fb/send-message sender-id (fb/text-message success-text))
+      :else (fb/send-message sender-id (fb/text-message (rand-nth wrong-texts))))))
 
 (defn on-postback [payload]
   (println "on-postback payload:")
@@ -26,8 +52,10 @@
         postback (get-in payload [:postback :payload])
         referral (get-in payload [:postback :referral :ref])]
     (cond
-      (= postback "GET_STARTED") (fb/send-message sender-id (fb/text-message "Welcome =)"))
-      :else (fb/send-message sender-id (fb/text-message "Sorry, I don't know how to handle that postback")))))
+      (= postback "GET_STARTED") (fb/send-message sender-id (fb/text-message (strone! Your result is correct. As we are always in need for talents like you, make the decision to apply today. We offer a wide range of exciting roles here at ICA. Discover our very latest vacancies.))))))
+Lorenz
+ "Hello, I am Commander Murphy and I work for the Intergalactic Communication Agency (ICA). We received a message from outer space and need your help to decode it. The message is: " ciphertext
+      :else (fb/send-message sender-id (fb/text-message "Sorry, I don't know how to handle that postback"))
 
 (defn on-attachments [payload]
   (println "on-attachment payload:")
